a5d48f971c39dd887c24bc9190c9e455e5d775ee
Matty Caulfield <caulfield2@wisc.edu>
2018-11-07 20:04:21 -0600

    Modified Distance.java to correctly follow the clustering algorithm with complete linkage

--- src/Distance.java
+++ src/Distance.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Distance {
 
 	public static void main(String[] args) {
-		List<Cluster> clusterList = new ArrayList<Cluster>();
+		List<Cluster> clusterList = new ArrayList<>();
 		int numClusters = 2;
 		
 		// Create a cluster for each city in the problem statement
@@ -12,42 +12,50 @@ public class Distance {
 		clusterList.add(new Cluster(new City("Boston", -71, 42, true)));
 		clusterList.add(new Cluster(new City("Vancouver", -123, 49, false)));
 		clusterList.add(new Cluster(new City("Winnipeg", -97, 50, false)));
-		clusterList.add(new Cluster (new City("Montreal", -71, 46, false)));
+		clusterList.add(new Cluster (new City("Montreal", -74, 46, false)));
 		
 		int iteration = 1;
 		while (clusterList.size() > numClusters) {
-			double currMaxDistance = 0;
-			List<Cluster> currFarthestClusters = new ArrayList<Cluster>();
+			double currMinDistance = 1000000;
+			List<Cluster> currClosestClusters = new ArrayList<>();
 			
 			for (Cluster clusterA : clusterList) {
 				for(Cluster clusterB : clusterList) {
 					if (clusterA != clusterB) {
-						for(City cityA : clusterA.cities) {
+                        double maxDistanceBetweenClusters = 0;
+
+                        for(City cityA : clusterA.cities) {
 							for(City cityB : clusterB.cities) {
 								// Euclidean distance between cities
-								double distance = Math.sqrt(Math.pow((cityA.x - cityB.x), 2) + Math.pow((cityA.y - cityB.y), 2));
+								double distance =
+										Math.sqrt(Math.pow((cityA.x - cityB.x), 2) + Math.pow((cityA.y - cityB.y), 2));
+
 								// Logic for clustering by complete linkage - single linkage would be the opposite
-								if (distance > currMaxDistance) {
-									currMaxDistance = distance;
-									currFarthestClusters.clear();
-									currFarthestClusters.add(clusterA);
-									currFarthestClusters.add(clusterB);
+								if (distance > maxDistanceBetweenClusters) {
+                                    maxDistanceBetweenClusters = distance;
 								}
 							}
 						}
+
+						if (maxDistanceBetweenClusters < currMinDistance) {
+                            currClosestClusters.clear();
+                            currClosestClusters.add(clusterA);
+                            currClosestClusters.add(clusterB);
+                            currMinDistance = maxDistanceBetweenClusters;
+                        }
 					}
 				}
 			}
 			
-			// Combine the two farthest clusters
-			Cluster keep = currFarthestClusters.get(0);
-			Cluster delete = currFarthestClusters.get(1);
+			// Combine the two closest clusters
+			Cluster keep = currClosestClusters.get(0);
+			Cluster delete = currClosestClusters.get(1);
 			
 			keep.cities.addAll(delete.cities);
 			clusterList.remove(delete);
 			
 			System.out.println("Iteration " + iteration);
-			System.out.println("Max distance: " + currMaxDistance);
+			System.out.println("Min distance: " + currMinDistance);
 			
 			for (Cluster cluster : clusterList) {
 				for (City city : cluster.cities) {
@@ -77,7 +85,7 @@ public class Distance {
 		List<City> cities;
 		
 		public Cluster(City city) {
-			this.cities = new ArrayList<City>(Arrays.asList(city));
+			this.cities = new ArrayList<>(Arrays.asList(city));
 		}
 	}
 	
